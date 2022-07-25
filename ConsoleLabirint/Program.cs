40ccb009559717a5398cf7a2161425d472cbd37e
daniil-kiryanov <daniil.kiryanov@gmail.com>
2022-07-25 10:00:42 +0400

    added classes

--- ConsoleLabirint/Program.cs
+++ ConsoleLabirint/Program.cs
@@ -2,37 +2,13 @@
 
 namespace ConsoleLabirint
 {
-    class Program
+    class Program 
+
     {
         static void Main(string[] args)
         {
-            int[,] labyrint = new int[,]
-            {
-                {1,1,1,1,0,1,1,1,1,1},
-                {1,0,1,1,0,1,0,0,0,1},
-                {1,0,0,0,0,1,0,1,0,1},
-                {1,1,1,1,0,1,0,1,0,1},
-                {0,0,0,0,0,1,0,1,0,1},
-                {1,1,0,1,1,1,0,1,0,1},
-                {1,1,0,0,0,0,0,1,0,1},
-                {1,1,1,1,1,1,1,1,0,1},
-                {1,0,0,0,0,0,0,0,0,1},
-                {1,1,1,1,1,1,0,1,1,1}
-            };
-            while (true)
-            {
-                for (int i = 0; i < labyrint.GetLength(0); i++)
-                {
-                    for (int j = 0; j < labyrint.GetLength(1); j++)
-                    {
-                        if (labyrint[i, j] == 0) Console.Write("0");
-                        if (labyrint[i, j] == 1) Console.Write("1");
-
-                    }
-                    Console.WriteLine();
-                }
-                Console.ReadLine();
-            }
+            int[,] labyrint = ProgramInput.InputData();
+            ProgramOutput.OutputData(labyrint);
         }
     }
 }
---
+++ ConsoleLabirint/ProgramOutput.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleLabirint
+{
+    class ProgramOutput
+    {
+
+        public static void OutputData(int[,] labyrint)
+        {
+            while (true)
+            {
+                for (int i = 0; i < labyrint.GetLength(0); i++)
+                {
+                    for (int j = 0; j < labyrint.GetLength(1); j++)
+                    {
+                        Console.Write($"{labyrint[i, j]}");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+                break;
+            }
+        }
+    }
+}
